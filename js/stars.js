61b5c5407c1343b8d2ead0585821c3068e4bd3a0
user <angelice92@mail.ru>
2020-09-25 16:13:22 +0300

    исправила классы по БЭМ, добавила функцию по клику

--- js/stars.js
+++ js/stars.js
@@ -1,6 +1,14 @@
-$(".item").on("mouseenter", function () {
+
+$(".places__photos-countries__all-stars__item").on("click", function () {
   $(this).addClass("fas");
-  $(".item").on("mouseout", function () {
-    $(this).removeClass("fas");
-  });
 });
+
+
+$(".places__photos-countries__all-stars__item").on("mouseenter", function () {
+  $(this).addClass("fas");
+});
+$(".places__photos-countries__all-stars__item").on("mouseleave", function () {
+    $(this).removeClass("fas");
+
+});
+
