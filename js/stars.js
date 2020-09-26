1ec2ffcf493383995a1096f5721bf1ca7c018fb1
user <angelice92@mail.ru>
2020-09-26 13:21:54 +0300

    сделала проверку на дата атрибут селектед

--- js/stars.js
+++ js/stars.js
@@ -1,14 +1,20 @@
 
 $(".places__photos-countries__all-stars__item").on("click", function () {
-  $(this).addClass("fas");
-});
+    $(this).attr('data-selected 1');
 
+});
 
 $(".places__photos-countries__all-stars__item").on("mouseenter", function () {
-  $(this).addClass("fas");
-});
-$(".places__photos-countries__all-stars__item").on("mouseleave", function () {
-    $(this).removeClass("fas");
+    $(this).addClass("fas");
 
-});
+}).on("mouseleave", function () {
+    const hasClass = $('.places__photos-countries__all-stars').hasClass('data-selected 1');
+    if (typeof hasClass!== 1 && hasClass !== true)
+    {
+    }
+    else {
+        $(this).removeClass("fas");
+    }
+    });
+
 
