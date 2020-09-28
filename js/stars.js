6209c9e4f198af389bb9125e09a47e0a8d8fc058
user <angelice92@mail.ru>
2020-09-28 14:47:13 +0300

    сделала ряд звездочек, чтоб загарались при наведеении, при клике оставались яркими, при следующем клике гасли, исправила ошибки

--- js/stars.js
+++ js/stars.js
@@ -1,31 +1,21 @@
-
-$(".places__photos-countries__all-stars__item").on("click", function () {
-    const hasAttribute = parseInt($(this).attr('data-selected'));
-
-    $(this).attr('data-selected', 1);
-   if(!isNaN(hasAttribute)){
-       $(this).removeClass("fas")
+$(".places__photos-countries__all-stars__item")
+  .on("click", function () {
+    const hasAttribute = parseInt($(this).attr("data-selected"));
+    if (hasAttribute) {
+      $(this).attr("data-selected", 0);
+    } else {
+      $(this).attr("data-selected", 1);
     }
   })
-    .on("mouseenter", function () {
-            $(this).addClass("fas");
+  .on("mouseenter", function () {
+    $(this).addClass("fas");
+  })
+  .on("mouseleave", function () {
+    const hasAttribute = parseInt($(this).attr("data-selected"));
 
-})
-.on("mouseleave", function () {
-
-    const hasAttribute = parseInt($(this).attr('data-selected'));
-
-    if(!hasAttribute){
-        $(this).addClass("fas");
+    if (!hasAttribute) {
+      $(this).removeClass("fas");
     }
 
-    if(!hasAttribute){
-        $(this).removeClass("fas")
-}
-
     console.log(hasAttribute);
-
-
-
-});
-
+  });
