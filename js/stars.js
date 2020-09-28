cf393fd33fee66b62d604d5ac2db7da80f4f4875
user <angelice92@mail.ru>
2020-09-28 10:39:18 +0300

    сделала ряд звездочек, чтоб загарались при наведеении, при клике оставались яркими, при следующем клике гасли

--- js/stars.js
+++ js/stars.js
@@ -1,19 +1,31 @@
 
 $(".places__photos-countries__all-stars__item").on("click", function () {
-    $(this).attr('data-selected');
-})
-
-.on("mouseenter", function () {
-    $(this).addClass("fas");
-
-}).on("mouseleave", function () {
-    let hasClass = $('.places__photos-countries__all-stars').hasClass('fas');
     const hasAttribute = parseInt($(this).attr('data-selected'));
-    if (typeof hasAttribute !== typeof undefined && hasAttribute !== false) {
-        $(this).removeClass('fas');
-    } else {
 
+    $(this).attr('data-selected', 1);
+   if(!isNaN(hasAttribute)){
+       $(this).removeClass("fas")
     }
+  })
+    .on("mouseenter", function () {
+            $(this).addClass("fas");
+
+})
+.on("mouseleave", function () {
+
+    const hasAttribute = parseInt($(this).attr('data-selected'));
+
+    if(!hasAttribute){
+        $(this).addClass("fas");
+    }
+
+    if(!hasAttribute){
+        $(this).removeClass("fas")
+}
+
+    console.log(hasAttribute);
+
+
 
 });
 
