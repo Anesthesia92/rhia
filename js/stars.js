9084f4f35acce6671eecd2b05e009925cc382d75
user <angelice92@mail.ru>
2020-09-29 15:42:20 +0300

    сделала рейтинг из звездочек при наведении и отведении мыши

--- js/stars.js
+++ js/stars.js
@@ -1,21 +1,31 @@
-$(".places__photos-countries__all-stars__item")
-  .on("click", function () {
-    const hasAttribute = parseInt($(this).attr("data-selected"));
-    if (hasAttribute) {
-      $(this).attr("data-selected", 0);
-    } else {
-      $(this).attr("data-selected", 1);
-    }
-  })
-  .on("mouseenter", function () {
-    $(this).addClass("fas");
-  })
-  .on("mouseleave", function () {
-    const hasAttribute = parseInt($(this).attr("data-selected"));
+//$(".places__photos-countries__all-stars__item")
+ // .on("click", function () {
+  ///  const hasAttribute = parseInt($(this).attr("data-selected"));
+  //  if (hasAttribute) {
+  //    $(this).attr("data-selected", 0);
+ //   } else {
+   //   $(this).attr("data-selected", 1);
+  //  }
+ // })
+ // .on("mouseenter", function () {
+  //  $(this).addClass("fas");
+ // })
+ // .on("mouseleave", function () {
+  //  const hasAttribute = parseInt($(this).attr("data-selected"));
 
-    if (!hasAttribute) {
-      $(this).removeClass("fas");
-    }
+  //  if (!hasAttribute) {
+   //   $(this).removeClass("fas");
+  //  }
+ // });
 
-    console.log(hasAttribute);
-  });
+$('.places__photos-countries__all-stars__item').hover(
+
+    function() {
+        $(this).prevAll().andSelf().addClass('fas');
+
+    },
+    function() {
+        $(this).prevAll().andSelf().removeClass('fas');
+        set_votes($(this).parent());
+    }
+);
