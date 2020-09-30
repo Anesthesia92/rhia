2bac08837db8961032a718649caea06151f49414
user <angelice92@mail.ru>
2020-09-30 16:52:55 +0300

    использовала метод children()

--- js/stars.js
+++ js/stars.js
@@ -1,27 +1,18 @@
+$(".places__photos-countries__all-stars__item")
+  .on("mouseenter", function () {
+    $(this).addClass("fas");
+    let starIndex = $(this).index("");
+    $("div")
+      .children(
+        ".places__photos-countries__all-stars__item:lt(" + starIndex + ")"
+      )
+      .addClass("fas");
+  })
 
-let starsSum = $('.places__photos-countries__all-stars__item .fas').length;
-let Stars = function(e) {
-    $('.places__photos-countries__all-stars__item').removeClass('fas');
-    for (let i = 0; e + 1 > i; i++) {
-        $('.places__photos-countries__all-stars__item').eq(i).toggleClass('fas');
-    }
-
-};
-
-$('.places__photos-countries__all-stars__item').on("mouseenter", function() {
-    Stars($(this).index());
-})
-
-.on("click", function () {
-    Stars($(this).index());
-    starsSum = $('.places__photos-countries__all-stars__item .fas').length;
-
-})
-.on('mouseleave', function() {
-    Stars(+starsSum - 1);
-});
-
-
-
-
-
+  .on("mouseleave", function () {
+    $("div")
+      .children(".places__photos-countries__all-stars__item")
+      .removeClass("fas");
+  });
+//.on("click", function () {
+//  Stars($(this).index());
