77c53b55f3ebaa1014bf014c5ebb65a5864b752c
user <angelice92@mail.ru>
2020-10-05 16:10:03 +0300

    исправила цикл .each

--- js/stars.js
+++ js/stars.js
@@ -1,26 +1,17 @@
 $(".places__photos-countries__all-stars__item")
   .on("mouseenter", function () {
-    $(this).addClass("fas");
     let starIndex = $(this).index(".places__photos-countries__all-stars__item");
-    $(".places__photos-countries__all-stars__item").each(function(i, item){
-     if (starIndex >= i) {
-
-       $(".places__photos-countries__all-stars")
-           .children(
-               ".places__photos-countries__all-stars__item:lt(" + starIndex + ")"
-           )
-           .addClass("fas");}
-      console.debug(i, item);
-    })
-
+    $(".places__photos-countries__all-stars__item").each(function (i, item) {
+      if (starIndex >= i) {
+        console.log(item);
+          $(item).addClass("fas");
+      }
+    });
   })
-
   .on("mouseleave", function () {
     const hasAttribute = parseInt($(this).attr("data-selected"));
     if (!hasAttribute) {
-      $(".places__photos-countries__all-stars")
-        .children(".places__photos-countries__all-stars__item")
-        .removeClass("fas");
+        $(".places__photos-countries__all-stars__item").removeClass("fas");
     }
   })
   .on("click", function () {
